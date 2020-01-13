53ee0313db30299491d20f03137ef50ab6dcd460
yanis <yanis.hammada@ricardo.ch>
2020-01-13 22:20:23 +0100

    Restore prometheus default metrics

--- src/api-routes/platform/index.ts
+++ src/api-routes/platform/index.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express'
 import prometheus from 'prom-client'
 
 const router: Router = Router()
+prometheus.collectDefaultMetrics()
 
 router.get('/healthz', (_req: Request, res: Response) => {
   res.status(200)
